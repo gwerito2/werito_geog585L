a2ef479c80872b38ad2906f995e7a013b33017ce
gwerito2 <gwerito2@unm.edu>
2018-05-07 22:47:00 -0600

    commit

--- final.js
+++ final.js
@@ -1,59 +1,60 @@
-   // var lon = -106.629267;
-   // var lat = 35.105445;
-   // var zoom = 12;
-   // var map, gphy, gmap, gter;
-
-//Set Controls
-  // var baseControls = [
-  // new OpenLayers.Control.PanZoom({}),
-  // new OpenLayers.Control.ScaleLine({}),
-  // new OpenLayers.Control.Scale(),
-  // new OpenLayers.Control.OverviewMap({})
-  // ];
-
-//Initialize function
-function init(){
-
-  var scaleLineControl = new ol.control.ScaleLine();
 
 
+  var sourceBingMaps = new ol.source.BingMaps({
+    key: 'AhwSYoaOsn_O2z7569TCYGIWnjUVg4Fk3wN9OehNGUDuPT0Z3OqRvEzlTiHz90K-',
+    imagerySet: 'Road',
+    culture: 'fr-FR'
+  });
+
+  var bingMapsAerial = new ol.layer.Tile({
+    preload: Infinity,
+    source: new ol.source.BingMaps({
+      key: 'AhwSYoaOsn_O2z7569TCYGIWnjUVg4Fk3wN9OehNGUDuPT0Z3OqRvEzlTiHz90K-',
+      imagerySet: 'Aerial',
+    })
+  });
+
+
+var Accident09 = new ol.layer.Tile({
+title:'2009 Accidents',
+source: new ol.source.TileWMS({
+url: 'http://internetmapping.net:8080/geoserver/wms?',
+  params: {
+    LAYERS: 'ws_gwerito2:2009 points',
+    FORMAT: 'image/png',
+    TRANSPARENT: true
+    },
+  })
+})
+var Accident08 = new ol.layer.Tile({
+title:'2008 Accidents',
+source: new ol.source.TileWMS({
+url: 'http://internetmapping.net:8080/geoserver/wms?',
+  params: {
+    LAYERS: 'ws_gwerito2:gw_2008_Accidents',
+    FORMAT: 'image/png',
+    TRANSPARENT: true
+    },
+  })
+})
+var POI = new ol.layer.Tile({
+title:'Establishments',
+source: new ol.source.TileWMS({
+url: 'http://internetmapping.net:8080/geoserver/wms?',
+  params: {
+    LAYERS: 'ws_gwerito2:POI',
+    FORMAT: 'image/png',
+    TRANSPARENT: true
+    },
+  })
+})
 
   var map = new ol.Map({
-        controls: ol.control.defaults({
-            attributionOptions: {
-              collapsible: false
-          }
-        }),
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
+    layers: [bingMapsAerial,Accident09, Accident08, POI],
+    target: 'map',
+    view: new ol.View({
+      center: ol.proj.transform([-106.629306, 35.105163], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 12
+       })
 
-        target: 'map',
-        view: new ol.View({
-          center: ol.proj.fromLonLat([-106.629306, 35.105163]),
-          zoom: 12
-        })
-      });
-
-
-
-  var point08 = new ol.layer.Tile({
-  		source: new ol.source.TileWFS({
-  		url: 'http://internetmapping.net:8080/geoserver/wfs?',
-  			params: {
-  				LAYERS: 'ws_gwerito2:gw_2008_Accidents',
-  				FORMAT: '.shp',
-  				// TRANSPARENT: true
-  				},
-  			})
-  		})
-  // http://internetmapping.net:8080/geoserver/wfs/general/user012/New folder/2008_Accidents/2008_Accidents.shp
-
-map.addLayers([point08,scaleLineControl]);
-
-map.addControl(new ol.Control.LayerSwitcher());
-
-map.addControl(new ol.control.ScaleLine({units: 'us'}));
-}
+  });
